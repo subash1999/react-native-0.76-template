68913f2423479decbd2a1581c21e731f4ca9c0d2
Subash Niroula <25361033+subash1999@users.noreply.github.com>
2025-01-04 19:56:00 +0900

    template fix for the conflict due to module:react-native-dotenv

--- babel.config.js
+++ babel.config.js
@@ -2,23 +2,28 @@ module.exports = function (api) {
     api.cache(true);
     return {
       presets: ['babel-preset-expo'],
-      plugins: [
-        [
-          "module:react-native-dotenv",
-          {
-            envName: "APP_ENV",
-            moduleName: "@env",
-            path: `./env/.env.${process.env.APP_ENV || "local"}`, // Load the environment file dynamically
-            blocklist: null,
-            allowlist: null,
-            blacklist: null, // DEPRECATED
-            whitelist: null, // DEPRECATED
-            safe: false,
-            allowUndefined: true,
-            verbose: false,
-          },
-        ],
-      ],
+      // this is commented out because of conflict with expo
+      // refer to this thread on github, will use this after issue is fixed
+      // https://github.com/goatandsheep/react-native-dotenv/issues/501
+      // alternative solution proposed here
+      // https://github.com/cchoi94/react-native-dotenv
+      // plugins: [
+      //   [
+      //     "module:react-native-dotenv",
+      //     {
+      //       envName: "APP_ENV",
+      //       moduleName: "@env",
+      //       path: `./.env.${process.env.APP_ENV || "local"}`, // Load the environment file dynamically
+      //       blocklist: null,
+      //       allowlist: null,
+      //       blacklist: null, // DEPRECATED
+      //       whitelist: null, // DEPRECATED
+      //       safe: false,
+      //       allowUndefined: true,
+      //       verbose: false,
+      //     },
+      //   ],
+      // ],
     };
   };
   
